42f3cc251ba274a35145ea80a80fb6ef069c519d
Leisan Gilfanova <lisynok@yandex-team.ru>
2017-03-18 17:55:44 +0300

    react

--- app/js/main.js
+++ app/js/main.js
@@ -9,70 +9,48 @@ var moduleShop = (function () {
     };
 
     var _setUpListeners = function () {
-        $('.filters__title.active').hover(_showOpen, _hideOpen);
-        $('.filters__title.no_active').hover(_showClose, _hideClose);
-        $('.filters__title').on('click', _accordeon);
+        $('.filter__title').on('click', _accordeon);
         $('.color__link').on('click', _colorChange);
-        $(".reset-filter").on('click', _resetCheckbox);
+        $('.reset-filter').on('click', _resetCheckbox);
         $('.sort__view').on('click', _changeSortView);
         $('.slideshow_pic').on('click', _slideShow);
-    };
-    
-    // Иконка при наведении на раздел аккордеона
-    var _showOpen = function () {
-      $('.open_filters').remove();      
-      $(this).prepend('<i class="close_filters"></i>');
-    };
-    var _hideOpen = function () {
-      $('.close_filters').remove();
-    };
-    var _showClose = function () {
-      $('.close_filters').remove();     
-      $(this).prepend('<i class="open_filters"></i>');
-    };
-    var _hideClose = function () {
-      $('.open_filters').remove();
     };
 
     // Аккордеон
     var _accordeon = function (e) {
-      e.preventDefault();
+        e.preventDefault();
 
-      var $this = $(this),
-          item = $this.closest('.basic__item'),
-          content = item.find('.basic_inner'),
-          duration = 300;
+        var $this = $(this),
+            item = $this.closest('.filters__item'),
+            content = item.find('.filter'),
+            duration = 300;
 
-      if($this.hasClass('no_active')) {
-        $this.removeClass('no_active');
-        $this.addClass('active');
-        content.stop(true, true).slideDown(duration);
-        $('.filters__title.active').hover(_showOpen, _hideOpen);        
-      } else { 
-        content.stop(true, true).slideUp(duration);
-        $this.removeClass('active');
-        $this.addClass('no_active');
-        $('.filters__title.no_active').hover(_showClose, _hideClose);     
-      };
+        if($this.hasClass('active')) {
+            content.stop(true, true).slideUp(duration);
+        } else {
+            content.stop(true, true).slideDown(duration);
+        }
+
+        $this.toggleClass('active');
     };
 
     // Фильтр по цветам
     var _colorChange = function (e) {
       e.preventDefault();
-      $(this).hasClass("choosen")?$(this).removeClass("choosen"):$(this).addClass("choosen");
+      $(this).toggleClass('choosen');
     };
 
     //очистка чекбоксов
     var _resetCheckbox = function (e) {
-      e.preventDefault();
+        e.preventDefault();
 
-      var $this = $(this),
-          item = $this.siblings('.checkbox__item'),
-          inputs = item.find('input:checkbox');
+        var $this = $(this),
+            item = $this.siblings('.checkbox__item'),
+            inputs = item.find('input:checkbox');
 
-      $.each(inputs, function() {
-        inputs.removeAttr("checked");
-      });
+        $.each(inputs, function() {
+            inputs.removeAttr('checked');
+        });
     };
 
     // Вид продукции
@@ -86,8 +64,9 @@ var moduleShop = (function () {
           item = $(this).children(),
           itemPosition = items.index(),
           viewActiveClass = 'sort__view'+ itemPosition +'-hover',
-          content = $('.content').children('ul'),
+          content = $('#content').find('ul').eq(0),
           contentClass = 'products_list'+ itemPosition +'';
+
       //меняем вид продукции и добавляем активный класс
       if(!content.hasClass(contentClass)){
           content.removeClass();
@@ -98,18 +77,18 @@ var moduleShop = (function () {
 
     // Слайдер с ценой
     var _slider = function() {
-      $( ".price_range__slider" ).slider({
+      $( '.price-range__slider' ).slider({
           range: true,
           min: 0,
           max: 30000,
           values: [ 700, 13000 ],
           slide: function( event, ui ) {
-            $( "#price_range1" ).val(ui.values[ 0 ]);
-            $( "#price_range2" ).val(ui.values[ 1 ]);
+            $( '#price-range1' ).val(ui.values[ 0 ]);
+            $( '#price-range2' ).val(ui.values[ 1 ]);
           }
         });
-        $( "#price_range1" ).val(  $( ".price_range__slider" ).slider( "values", 0 ));
-        $( "#price_range2" ).val(  $( ".price_range__slider" ).slider( "values", 1 ));
+        $( '#price-range1' ).val(  $( '.price-range__slider' ).slider( 'values', 0 ));
+        $( '#price-range2' ).val(  $( '.price-range__slider' ).slider( 'values', 1 ));
     };
 
     // Две колонки текста
@@ -119,7 +98,7 @@ var moduleShop = (function () {
 
     // select
     var  _select = function() {
-      $(".js-example-basic-single").select2({
+      $('.js-example-basic-single').select2({
           minimumResultsForSearch: Infinity
       });
     }
@@ -146,7 +125,7 @@ var moduleShop = (function () {
     var _firstCome = function() {
       var items = $('.mini__list');
       for (var i = 0; i < items.length; i++) {
-        $('.mini__list')[i].children[0].setAttribute("class", "mini__item mini__item-active")
+        $('.mini__list')[i].children[0].setAttribute('class', 'mini__item mini__item-active')
       }
     }
 
@@ -156,5 +135,5 @@ var moduleShop = (function () {
     
 }) ();
 
-moduleShop.init();
+window.onload = moduleShop.init();
 
---
+++ app/js/phones.js
@@ -0,0 +1,312 @@
+var PHONES = [
+    {
+    name :'Samsung GT-I9301I  GALAXY S 3',
+    image : 'images/img/Samsung-GT-I9301I-s1.jpg',
+    images: [{image: 'images/img/Samsung-GT-I9301I-s1.jpg'},
+        {image: 'images/img/Samsung-GT-I9301I-s2.jpg'},
+        {image: 'images/img/Samsung-GT-I9301I-s3.jpg'}],
+    articul :'128789',
+    feature : [
+        {name: 'Тип SIM-карты', value: 'Micro-SIM'},
+        {name: 'Количество SIM-карт', value: 1},
+        {name: 'Цвет', value : 'Белый'},
+        {name: 'ОС', value: 'Android'},
+        {name: 'Связь 3G', value : 'Есть'},
+        {name: '4G(LTE)', value : 'Нет'},
+        {name: 'GPS-модуль', value : 'Есть'}
+    ],
+    ratio : 3,
+    nalichie : 'В наличии',
+    info : 'Смартфон Samsung GT-I9301I GALAXY S 3 Ceramic White Моноблок, Micro-SIM, Android 4.4, 3G, Bluetooth 4.0, EDGE, GPRS, WAP, Wi-Fi, Количество ядер процессора 4, Частота процессора 1400 МГц...',
+    price : '11 290'
+}, {
+    name :'Microsoft Lumia 640 RM-1077',
+    image : 'images/img/Microsoft-Lumia-640-s1.jpg',
+    images: [{image: 'images/img/Microsoft-Lumia-640-s1.jpg'},
+        {image: 'images/img/Microsoft-Lumia-640-s2.jpg'},
+        {image: 'images/img/Microsoft-Lumia-640-s3.jpg'}],
+    articul :'321559',
+    feature : [
+        {name: 'Тип SIM-кары', value: 'Micro-SIM'},
+        {name: 'Количество SIM-карт', value: 2},
+        {name: 'Цвет', value : 'Белый'},
+        {name: 'ОС', value: 'Windows Phone'},
+        {name: 'Связь 3G', value: 'Есть'},
+        {name: '4G(LTE)', value: 'Нет'},
+        {name: 'GPS-модуль', value: 'A-GPS'}
+    ],
+    ratio : 5,
+    nalichie : 'В наличии',
+    info : 'Смартфон Microsoft Lumia 640 Dual SIM (RM-1077) С ним вы открываете мир ярких цифровых возможностей. Этот многофункциональный смартфон уже содержит набор бесплатных сервисов Microsoft...',
+    price : '10 990'
+}, {
+    name :'Archos 50 Oxygen Plus',
+    image : 'images/img/Archos-50-Oxygen-Plus-s1.jpg',
+    images: [{image: 'images/img/Archos-50-Oxygen-Plus-s1.jpg'},
+        {image: 'images/img/Archos-50-Oxygen-Plus-s2.jpg'},
+        {image: 'images/img/Archos-50-Oxygen-Plus-s3.jpg'}],
+    articul :'132489',
+    feature : [
+        {name: 'Тип SIM-кары', value: 'Micro-SIM'},
+        {name: 'Количество SIM-карт', value: 2},
+        {name: 'Цвет', value: 'Серый'},
+        {name: 'ОС', value: 'Android'},
+        {name: 'Связь 3G', value: 'Есть'},
+        {name: '4G(LTE)', value: 'Нет'},
+        {name: 'GPS-модуль', value: 'Есть'}
+    ],
+    ratio : 4,
+    nalichie : 'В наличии',
+    info : 'Смартфон Archos 50 Oxygen Plus, серый. Моноблок, Micro-SIM, Android , 3G, Bluetooth, USB-подключение к ПК, Wi-Fi, Разрешение основной камеры 8 Мп...',
+    price : '9 900'
+}, {
+    name :'Philips S398 Black',
+    image : 'images/img/Philips-S398-Black-s1.jpg',
+    images: [{image: 'images/img/Philips-S398-Black-s1.jpg'},
+        {image: 'images/img/Philips-S398-Black-s2.jpg'},
+        {image: 'images/img/Philips-S398-Black-s3.jpg'}],
+    articul :'321559',
+    feature : [
+        {name: 'Тип SIM-карты', value: null},
+        {name: 'Количество SIM-карт', value: 2},
+        {name: 'Цвет', value : null},
+        {name: 'ОС', value: 'Android'},
+        {name: 'Связь 3G', value: null},
+        {name: '4G(LTE)', value: 'Нет'},
+        {name: 'GPS-модуль', value: null}
+    ],
+    ratio : 2,
+    nalichie : 'В наличии',
+    info : 'Смартфон Philips S398 Black. Черный корпус и разнообразие цветных панелей для Philips S398 как нельзя лучше подчеркнут ваш статус и уверенность в себе. Преимущества этой модели...',
+    price : '8 990'
+}, {
+    name :'Microsoft Lumia 430 RM-10',
+    image : 'images/img/Microsoft-Lumia-430-RM-10-s1.jpg',
+    images: [{image: 'images/img/Microsoft-Lumia-430-RM-10-s1.jpg'},
+        {image: 'images/img/Microsoft-Lumia-430-RM-10-s2.jpg'},
+        {image: 'images/img/Microsoft-Lumia-430-RM-10-s3.jpg'}],
+    articul :'965471',
+    feature : [
+        {name: 'Тип SIM-кары', value: 'Micro-SIM'},
+        {name: 'Количество SIM-карт', value: 2},
+        {name: 'Цвет', value : 'Черный'},
+        {name: 'ОС', value: 'Windows Phone'},
+        {name: 'Связь 3G', value: 'Есть'},
+        {name: '4G(LTE)', value: 'Нет'},
+        {name: 'GPS-модуль', value: 'A-GPS'}
+    ],
+    ratio : 4,
+    nalichie : 'В наличии',
+    info : 'Смартфон Microsoft Lumia 430 RM-1099 black Куда бы вы ни направлялись, смартфон должен быть надежным спутником. Поэтому у Microsoft Lumia 430 Dual SIM (RM-1099) удобный компактный корпус ...',
+    price : '5 390'
+}, {
+    name :'Senseit L301 Black ',
+    image : 'images/img/Senseit-L301-Black-s1.jpg',
+    images: [{image: 'images/img/Senseit-L301-Black-s1.jpg'},
+        {image: 'images/img/Senseit-L301-Black-s2.jpg'},
+        {image: 'images/img/Senseit-L301-Black-s3.jpg'}],
+    articul :'321419',
+    feature : [
+        {name: 'Тип SIM-кары', value: 'Micro-SIM'},
+        {name: 'Количество SIM-карт', value: 2},
+        {name: 'Цвет', value: 'Черный'},
+        {name: 'ОС', value: 'Android'},
+        {name: 'Связь 3G', value: 'Есть'},
+        {name: '4G(LTE)', value: 'Нет'},
+        {name: 'GPS-модуль', value : 'A-GPS'}
+    ],
+    ratio : 1,
+    nalichie : 'В наличии',
+    info : 'Смартфон Senseit L301 black, черный. Micro-SIM, две поддерживаемых SIM-карты, Android, 3G, GPS-модуль...  ',
+    price : '4 990'
+}, {
+    name :'HIGHSCREEN WinWin Black',
+    image : 'images/img/HIGHSCREEN-WinWin-Black-s1.jpg',
+    images: [{image: 'images/img/HIGHSCREEN-WinWin-Black-s1.jpg'},
+        {image: 'images/img/HIGHSCREEN-WinWin-Black-s2.jpg'},
+        {image: 'images/img/HIGHSCREEN-WinWin-Black-s3.jpg'}],
+    articul :'451781',
+    feature : [
+        {name: 'Тип SIM-кары', value: 'Micro-SIM'},
+        {name: 'Количество SIM-карт', value: 2},
+        {name: 'Цвет', value : 'Черный'},
+        {name: 'ОС', value: 'Windows Phone'},
+        {name: 'Связь 3G', value: 'Есть'},
+        {name: '4G(LTE)', value: 'Нет'},
+        {name: 'GPS-модуль', value: 'Есть'}
+    ],
+    ratio : 5,
+    nalichie : 'В наличии',
+    info : 'Смартфон HIGHSCREEN WinWin Black оснащен 4" дисплеем и двумя слотами для SIM-карт.  Windows Phone , 3G, Bluetooth, USB-подключение к ПК...',
+    price : '3 990'
+}, {
+    name :'Alcatel OT4013D PIXI 3 (2 SIM)',
+    image : 'images/img/Alcatel-OT4013D-PIXI3-(2SIM)-s1.jpg',
+    images: [{image: 'images/img/Alcatel-OT4013D-PIXI3-(2SIM)-s1.jpg'},
+        {image: 'images/img/Alcatel-OT4013D-PIXI3-(2SIM)-s2.jpg'},
+        {image: 'images/img/Alcatel-OT4013D-PIXI3-(2SIM)-s3.jpg'}],
+    articul :'321489',
+    feature : [
+        {name: 'Тип SIM-кары', value: 'Micro-SIM'},
+        {name: 'Количество SIM-карт', value: 2},
+        {name: 'Цвет', value: 'Белый'},
+        {name: 'ОС', value: 'Android'},
+        {name: 'Связь 3G', value: 'Есть'},
+        {name: '4G(LTE)', value: 'Нет'},
+        {name: 'GPS-модуль', value : 'A-GPS'}
+    ],
+    ratio : 4,
+    nalichie : 'В наличии',
+    info : 'СмартфонAlcatel OT4013D PIXI 3 (2 SIM). Смартфон Alcatel One Touch 4013D оснащен четырехдюймовым дисплеем и двумя слотами для SIM-карт...',
+    price : '3 990'
+}, {
+    name :'Fly IQ434 Era Nano 5',
+    image : 'images/img/Fly-IQ434-Era-Nano-5-s1.jpg',
+    images: [{image: 'images/img/Fly-IQ434-Era-Nano-5-s1.jpg'},
+        {image: 'images/img/Fly-IQ434-Era-Nano-5-s2.jpg'},
+        {image: 'images/img/Fly-IQ434-Era-Nano-5-s3.jpg'}],
+    articul :'987441',
+    feature : [
+        {name: 'Тип SIM-кары', value: 'Micro-SIM'},
+        {name: 'Количество SIM-карт', value: 2},
+        {name: 'Цвет', value: 'Черный'},
+        {name: 'ОС', value: 'Android'},
+        {name: 'Связь 3G', value : 'Нет'},
+        {name: '4G(LTE)', value : 'Нет'},
+        {name: 'GPS-модуль', value : 'Нет'}
+    ],
+    ratio : 3,
+    nalichie : 'В наличии',
+    info : 'Смартфон Fly IQ434 Era Nano 5 оснащен 3.5" дисплеем и двумя слотами для SIM-карт. Android 2.3, Micro SD, Wi-Fi.... ',
+    price : '1 790'
+}];
+
+var MiniImage = React.createClass({
+    render: function() {
+        return (
+            <li className="mini__item">
+                <a href="#" className="slideshow_pic">
+                    <img className="mini__img" src={this.props.image} />
+                </a>
+            </li>
+        );
+    }
+});
+
+var Feature = React.createClass({
+    render: function() {
+        return (
+            <li className="features__item">
+                <div className="features_name">{this.props.name}</div>
+                <div className="features_value">{this.props.value}</div>
+            </li>
+        );
+    }
+});
+
+var Contact = React.createClass({
+    getInitialState: function() {
+        return {
+            miniImages: this.props.images,
+            features: this.props.feature
+        };
+    },
+
+    render: function() {
+        return (
+            <li className="products">
+                <div className="products__slideshow">
+                    <div className="slideshow">
+                        <img className="big-photo" src={this.props.image} alt={this.props.name} />
+                    </div>
+                    <ul className="mini__list">
+                        {
+                            this.state.miniImages.map((el, i) => {
+                                return <MiniImage
+                                    key={i}
+                                    image={el.image}
+                                />;
+                            })
+                        }
+                    </ul>
+                    <div className="products__articul">
+                        <span className="art"> Артикул </span>
+                        <span className="art_number">{this.props.articul}</span>
+                    </div>
+                </div>
+                <div className="products_center_content">
+                    <div className="products__title">{this.props.name}</div>
+                    <div className="products__info">{this.props.info}</div>
+                    <div className="products__feature">
+                        <ul className="features__list">
+                            {
+                                this.state.features.map((el, i) => {
+                                    return <Feature
+                                        key={i}
+                                        name={el.name}
+                                        value={el.value}
+                                    />;
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className="products__reiting">
+                        <div className={"reiting__stars stars-" + this.props.ratio}>
+                            <ul className="stars">
+                                <li className="star"></li>
+                                <li className="star"></li>
+                                <li className="star"></li>
+                                <li className="star"></li>
+                                <li className="star"></li>
+                            </ul>
+                        </div>
+                        <div className="reiting__value">{this.props.ratio}</div>
+                    </div>
+
+                </div>
+            <div className="products_right_content">
+                <div className="products__price">{this.props.price} P</div>
+                <div className="products_nallichie">{this.props.nalichie}</div>
+                <button className="products__buy">В корзину</button>
+            </div>
+        </li>
+        );
+    }
+});
+
+var ContactsList = React.createClass({
+    getInitialState: function() {
+        return {
+            displayedContacts: PHONES
+        };
+    },
+    render: function() {
+        return (
+            <div className="contacts">
+                <ul className="products_list1">
+                    {
+                        this.state.displayedContacts.map((el, i) => {
+                            return <Contact
+                                key={i}
+                                name={el.name}
+                                info={el.info}
+                                image={el.image}
+                                price={el.price}
+                                nalichie={el.nalichie}
+                                ratio={el.ratio}
+                                articul={el.articul}
+                                images={el.images}
+                                feature={el.feature}
+                            />;
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <ContactsList />,
+    document.getElementById("content")
+);
